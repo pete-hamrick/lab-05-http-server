b2fa9cb48d245216eda440a3e3da78aa7f94966e
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-20 18:27:15 -0700

    fixed eslint and prettier to have 4 spaces on indents

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,10 @@ import { readFile } from 'fs/promises';
 import request from 'supertest';
 import app from '../lib/app.js';
 describe('app.js public routes', () => {
-  it.only('should return index.html from GET /', async () => {
-    const res = await request(app).get('/');
-    console.log(res.text);
-    const readHTML = await readFile('./public/index.html', 'utf-8');
-    expect(res.text).toEqual(readHTML);
-  });
+    it.only('should return index.html from GET /', async () => {
+        const res = await request(app).get('/');
+        console.log(res.text);
+        const readHTML = await readFile('./public/index.html', 'utf-8');
+        expect(res.text).toEqual(readHTML);
+    });
 });
--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -2,73 +2,73 @@
 import { rm, mkdir } from 'fs/promises';
 import SimpleDb from '../lib/simple-db.js';
 describe('simple db', () => {
-  const rootDir = './__tests__/store';
+    const rootDir = './__tests__/store';
 
-  beforeEach(async () => {
-    await rm(rootDir, { force: true, recursive: true });
-    await mkdir(rootDir, { recursive: true });
-  });
+    beforeEach(async () => {
+        await rm(rootDir, { force: true, recursive: true });
+        await mkdir(rootDir, { recursive: true });
+    });
 
-  xit('saved object has id', async () => {
-    const db = new SimpleDb(rootDir);
+    xit('saved object has id', async () => {
+        const db = new SimpleDb(rootDir);
 
-    const felix = { name: 'felix', type: 'tuxedo' };
+        const felix = { name: 'felix', type: 'tuxedo' };
 
-    await db.save(felix);
-    expect(felix.id).toEqual(expect.any(String));
-  });
+        await db.save(felix);
+        expect(felix.id).toEqual(expect.any(String));
+    });
 
-  xit('save and get an object', async () => {
-    const db = new SimpleDb(rootDir);
+    xit('save and get an object', async () => {
+        const db = new SimpleDb(rootDir);
 
-    const felix = { name: 'felix', type: 'tuxedo' };
-    await db.save(felix);
-    const got = await db.get(felix.id);
-    expect(got).toEqual(felix);
-  });
+        const felix = { name: 'felix', type: 'tuxedo' };
+        await db.save(felix);
+        const got = await db.get(felix.id);
+        expect(got).toEqual(felix);
+    });
 
-  xit('returns null for non-existant id', async () => {
-    const db = new SimpleDb(rootDir);
+    xit('returns null for non-existant id', async () => {
+        const db = new SimpleDb(rootDir);
 
-    const got = await db.get('non-existant');
-    expect(got).toBeNull();
-  });
+        const got = await db.get('non-existant');
+        expect(got).toBeNull();
+    });
 
-  xit('gets all objects', async () => {
-    const cats = [
-      { name: 'felix', type: 'tuxedo' },
-      { name: 'garfield', type: 'orange tabby' },
-      { name: 'duchess', type: 'angora' },
-    ];
+    xit('gets all objects', async () => {
+        const cats = [
+            { name: 'felix', type: 'tuxedo' },
+            { name: 'garfield', type: 'orange tabby' },
+            { name: 'duchess', type: 'angora' },
+        ];
 
-    const db = new SimpleDb(rootDir);
+        const db = new SimpleDb(rootDir);
 
-    await Promise.all(cats.map((cat) => db.save(cat)));
-    const got = await db.getAll();
-    expect(got).toEqual(expect.arrayContaining(cats));
-  });
+        await Promise.all(cats.map((cat) => db.save(cat)));
+        const got = await db.getAll();
+        expect(got).toEqual(expect.arrayContaining(cats));
+    });
 
-  xit('deletes an object', async () => {
-    const db = new SimpleDb(rootDir);
+    xit('deletes an object', async () => {
+        const db = new SimpleDb(rootDir);
 
-    const felix = { name: 'felix', type: 'tuxedo' };
+        const felix = { name: 'felix', type: 'tuxedo' };
 
-    await db.save(felix);
-    await db.delete(felix.id);
-    const got = await db.get(felix.id);
-    expect(got).toBeNull();
-  });
+        await db.save(felix);
+        await db.delete(felix.id);
+        const got = await db.get(felix.id);
+        expect(got).toBeNull();
+    });
 
-  xit('updates an object', async () => {
-    const db = new SimpleDb(rootDir);
+    xit('updates an object', async () => {
+        const db = new SimpleDb(rootDir);
 
-    const felix = { name: 'felix', type: 'tuxedo' };
-    await db.save(felix);
+        const felix = { name: 'felix', type: 'tuxedo' };
+        await db.save(felix);
 
-    felix.type = 'tabby';
-    await db.update(felix);
+        felix.type = 'tabby';
+        await db.update(felix);
 
-    const got = await db.get(felix.id);
-    expect(got).toEqual(felix);
-  });
+        const got = await db.get(felix.id);
+        expect(got).toEqual(felix);
+    });
 });
