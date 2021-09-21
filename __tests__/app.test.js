330c447c469afc8fc9fa87f0ed94fde3f6145bd2
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-20 20:14:59 -0700

    failing get stylesheet test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,14 @@ import { readFile } from 'fs/promises';
 import request from 'supertest';
 import app from '../lib/app.js';
 describe('app.js public routes', () => {
-    it.only('should return index.html from GET /', async () => {
+    it('should return index.html from GET /', async () => {
         const res = await request(app).get('/');
-        console.log(res.text);
         const readHTML = await readFile('./public/index.html', 'utf-8');
         expect(res.text).toEqual(readHTML);
+    });
+    it('should return main.css file contents on GET /styles/main.css', async () => {
+        const res = await request(app).get('/styles/main.css');
+        const css = await readFile('./public/css/main.css');
+        expect(res.text).toEqual(css);
     });
 });
